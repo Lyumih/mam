374c7b39c2fb239070a39b322ec8246dad211954
mikhail <mikhail@MacBook-Pro-mikhail.local>
2022-12-21 23:36:34 +0300

    mol

--- generated/generated.view.ts
+++ generated/generated.view.ts
@@ -3,7 +3,7 @@ namespace $.$$ {
 	const hash = ( s: string ) => {
 		return s.split( "" ).reduce( function( a, b ) { a = ( ( a << 5 ) - a ) + b.charCodeAt( 0 ); return a & a }, 0 )
 	}
-	export class $link_link extends $.$link_link {
+	export class $generated_link extends $.$generated_link {
 		urlLink() {
 			return `${ URL }/${ hash( this.link() ) }`
 		}
